6d6eb3fb14512188d7a362e16da5fe18b2e69ae2
Yilin Liu <yliu469@wisc.edu>
2017-09-17 12:16:11 -0500

    Update YLNet.py

--- YLNet.py
+++ YLNet.py
@@ -80,15 +80,15 @@ class YLNet(nn.Module):
         de_1 = self.decoder_1(en3_maxpool)#32x32x64
         de1_unpool = self.unpool_1(en3_maxpool, indices_3, output_size=size_3) #transfer of indices
         merge1_3 = torch.cat((de1_unpool,en_3), 1)#64x64x128
-        print 'merge1_3 ', merge1_3.size()
+        
         de_2 = self.decoder_2(merge1_3)#64x64x64
         de2_unpool = self.unpool_2(en2_maxpool, indices_2, output_size=size_2) #transfer of indices
         merge2_2 = torch.cat((de2_unpool,en_2), 1)#128x128x128
-        print 'merge2_2 ', merge2_2.size()
+       
         de_3 = self.decoder_3(merge2_2)#128x128x64
         de3_unpool = self.unpool_3(en1_maxpool, indices_1, output_size=size_1) #transfer of indices
         merge3_1 = torch.cat((de3_unpool,en_1), 1)#256x256x128
-        print 'merge3_1 ', merge3_1.size()
+        
         conv_4 = self.conv_4(merge3_1)#256x256x64
 
         output = Funct.softmax(conv_4)
